b3509c4731e909af5655e366a6b69beb23c1218f
dipayan003 <dipayan.dey.22@aot.edu.in>
2024-07-22 20:06:43 +0530

    took help from blackbox ai 1

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ function setupMap(center){
       
       map.addControl(directions, 'top-left');
 }
+// Add a marker to show the user's location
+const marker = new mapboxgl.Marker()
+.setLngLat(center)
+.addTo(map);
+
+// Add an event listener to the map to update the marker when the user moves
+map.on('move', () => {
+marker.setLngLat(map.getCenter());
+});
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        searchLocation(searchInput.value);
+    }
+});
+
+function searchLocation(query) {
+    // Use the Mapbox Geocoding API to search for the location
+    const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${mapboxgl.accessToken}`;
+    fetch(geocodingUrl)
+        .then(response => response.json())
+        .then(data => {
+            const coordinates = data.features[0].center;
+            setupMap(coordinates);
+        })
+        .catch(error => console.error(error));
+}
